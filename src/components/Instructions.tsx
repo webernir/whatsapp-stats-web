c60268b81bc8a1485d46a81d38f999a97d41045d
webernir@bina-fin.co.il <my.guru.pitka@gmail.com>
2017-10-22 07:27:34 +0300

    updated instructions

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
 import { Box, Content } from "bloomer";
 
-export interface InstructionsProps {}
+export interface InstructionsProps { }
 
 export default class Instructions extends React.Component<
   InstructionsProps,
   {}
-> {
+  > {
   render() {
     return (
       <Content>
         <Box>
           <h3>Getting Started:</h3>
           <strong>Your chat history is safe!</strong>
-          <span>&nbsp; it's not uploaded to anywehere.</span>
+          <span>&nbsp; it's not uploaded anywehere.</span>
           <hr />
           <p>
             To export a copy of the history of an individual chat or group, use
@@ -26,7 +26,7 @@ export default class Instructions extends React.Component<
             <li>Tap Email chat.</li>
             <li>Choose whether to Attach Media or not.</li>
             <li>
-              An email will be composed with your chat history attached as a 
+              An email will be composed with your chat history attached as a
               <code>.txt</code> document.
             </li>
           </ol>
